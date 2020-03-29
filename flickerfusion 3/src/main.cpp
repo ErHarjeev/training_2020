c35f5cae610f973b5a31adcadd4308642ecf3d93
Harjeev <er.harjeev@gmail.com>
2020-03-30 09:50:06 +1300

    program

--- flickerfusion 3/src/main.cpp
+++ flickerfusion 3/src/main.cpp
@@ -1,8 +1,36 @@
 #include <Arduino.h>
+const uint8_t Button = A0;
+bool status=0; 
 
-void setup() {
-  // pyour setup code here, to run once:
-}
+void setup() 
+  {
+  Serial.begin(9600);
+  pinMode(Button,INPUT_PULLUP);
+  pinMode(LED_BUILTIN,OUTPUT);
+  digitalWrite(LED_BUILTIN,LOW); // pyour setup code here, to run once:
+  }
 
 void loop() {
-  // p
+
+         
+        if (digitalRead(Button) == 0)
+        {
+            status = !status;
+            digitalWrite(LED_BUILTIN,status);
+            if (status == 1)
+            {
+              Serial.println("Led is On");
+
+            }
+            else
+            {
+              Serial.println("LED is OFF");
+            }
+
+            delay(500);
+            while(digitalRead(Button) == 0)
+            {;}
+        }
+
+}
+
